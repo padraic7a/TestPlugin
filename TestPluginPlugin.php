71d45a4f2086dec05acab538a0429f6c8b3abc6e
padraic7a <padraic.stack@gmail.com>
2015-10-22 10:08:25 +0100

    trying to put in  search

--- TestPluginPlugin.php
+++ TestPluginPlugin.php
@@ -1,21 +1,27 @@
 <?php
 
 class TestPluginPlugin extends Omeka_Plugin_AbstractPlugin
-{
-protected $_hooks = array('initialize');
-
-public function hookInitialize()
-{
-add_shortcode('test_plugin', array($this, 'shortcode'));
-}
 
 
-public function shortcode($args, $view)
-{
-return 'This is a very simple shortcode.';
-}
-}
-
+ {
+ protected $_hooks = array('initialize');
  
-?>
+ public function hookInitialize()
+ {
+ add_shortcode('test_plugin', array($this, 'shortcode'));
+ }
+ 
+ public function shortcode($sql, $view)
+{
+$sql = Omeka_Db_Select()
+             ->from('omeka_element_texts',array('DISTINCT(text)'))
+             ->where('element_id =37','element_id =39');
 
+return $sql;
+return 'Hello shortcode!';
+
+}
+
+}
+
+?>
